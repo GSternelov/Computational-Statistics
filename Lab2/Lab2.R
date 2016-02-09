f3e9651dfc482346f55fd4f845d20290f8839c10
sternis <gustav.sternelov@hotmail.com>
2016-02-09 22:32:59 +0100

    Working...

--- Lab2/Lab2.R
+++ Lab2/Lab2.R
@@ -1,3 +1,6 @@
+
+## Assignment 1 ## 
+# 1.1
 mort_rate<-read.csv2("C:\\Users\\Gustav\\Documents\\Computational-Statistics\\Lab2\\mortality_rate.csv",sep=";")
 mort_rate$LMR <- log(mort_rate$Rate)
 
@@ -6,6 +9,10 @@ set.seed(123456)
 id=sample(1:n, floor(n*0.5))
 train=mort_rate[id,]
 test=mort_rate[-id,]
+
+# 1.2
+Data <- list(X=train$Day, Y=train$LMR, Xtest=test$Day, Ytest=test$LMR)
+
 myMSE <- function(lambda, pars){
   model <- loess(pars$Y~pars$X, data=pars[1:2], enp.target = lambda)
   Pred <- predict(model, newdata=pars$Xtest)
@@ -13,7 +20,6 @@ myMSE <- function(lambda, pars){
   print(MSE)
   return(MSE)
 }
-Data <- list(X=train$Day, Y=train$LMR, Xtest=test$Day, Ytest=test$LMR)
 # 1.3 
 Lambda <-  seq(0.1,40, by=0.1)
 modelMSE <- 0
@@ -22,25 +28,39 @@ for(i in Lambda){
   j <- j+1
   modelMSE[j] <- myMSE(Lambda[j], Data)
 }
+
 library(ggplot2)
 MSEdata <- data.frame(MSE=modelMSE, lambda=Lambda)  
 ggplot(MSEdata, aes(x=lambda, y=MSE, label=lambda)) + geom_line(col="darkblue")+
-  geom_text(data=subset(MSEdata, MSE <= min(MSEdata$MSE))[1,],vjust=-0.6, size=8) + 
-  geom_point(data=subset(MSEdata, MSE <= min(MSEdata$MSE))[1,], size=3.5, col="darkorange")
+  geom_text(data=subset(MSEdata, MSE <= min(MSEdata$MSE))[1,],vjust=-0.6, size=10) + 
+  geom_point(data=subset(MSEdata, MSE <= min(MSEdata$MSE))[1,], size=5, col="darkorange")
+
+# 1.4
 Optim <- optimize(f=myMSE, interval = c(0.1, 40), tol=0.01, pars=Data)
-Optim
+
+# 1.5
 optim(par=35, fn=myMSE, method = "BFGS", pars=Data)
-ggplot(MSEdata, aes(x=lambda, y=MSE, label=lambda)) + geom_line(col="darkblue")+
-  geom_text(data=subset(MSEdata,  MSEdata$lambda == 35),vjust=-0.6, size=8) + 
-  geom_point(data=subset(MSEdata,  MSEdata$lambda == 35), size=3.5, col="darkorange")
+
+
+## Assignment 2 ##
+
 # 2.1
 load("C:\\Users\\Gustav\\Documents\\Computational-Statistics\\Lab2\\data.RData")
 Data2 <- data
+
+# 2.2
+# Log likelihood
+#(-n/2) * log(2*pi*sigma2) - (1/2*sigma2)*sum(x-my)^2
 # Maximum likelihood estimates
 # my
 MeanEst <- sum(Data2) / length(Data2)
-# sigma 
-sigmaEst <- sqrt((1/length(Data2)) * sum((Data2-MeanEst)^2))
+
+# sigma2 
+(1/length(Data2)) * sum((Data2-MeanEst)^2)
+
+
+# 2.3
+
 
 # 2.4
 # minus log-like function
@@ -51,6 +71,7 @@ loglikef <- function(par,...){
   loglike <- -(n*log(1/(sqrt(2*pi*sigma^2))) - (sum((data-my)^2)/(2*sigma^2)))
   return(loglike)
 }
+loglikef(c(0,1),Data2)
 
 gradientf <- function(par,...){
   n <- length(data)
@@ -60,8 +81,13 @@ gradientf <- function(par,...){
   mySigma <- - (n/2)*(2/sigma)+ 2*sigma - (2*sigma^2)^-2 * (-2*sum(data-my)) 
   return(c(myML, mySigma))
 }
+
 optim(par = c(0,1), fn=loglikef, method = "CG", data=Data2)
 optim(par = c(0,1), fn=loglikef, gr=gradientf,  method = "CG", data=Data2)
+
 optim(par = c(0,1), fn=loglikef , method = "BFGS", data=Data2)
 optim(par = c(0,1), fn=loglikef, gr=gradientf,  method = "BFGS", data=Data2)
-## NA
+
+
+
+
